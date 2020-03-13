68f534f3531fcc6f527ceed08a3103b55adbac60
Thomas <thomaschou861205@gmail.com>
2020-03-13 16:20:12 +0800

    - final ver (without comment) - executable only on Linux

--- hw01/hw01.c
+++ hw01/hw01.c
@@ -6,11 +6,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/time.h>
+#define METHOD 3
 
 int N;
 char **data;	// input data
 char **A;		//array to be sorted
 int R = 1;
+char algNames[4][10] = {"Selection", "Insertion", "Bubble", "Shaker"};
+void (*Sort[4])(char **list, int n);
 
 void readInput(void);
 void printArray(char **A);
@@ -21,6 +24,7 @@ void InsertionSort(char **list, int n);
 void BubbleSort(char **list, int n);
 void ShakerSort(char **list, int n);
 void swap(char **a, char **b);
+void freeMemory(char **list, int n);
 
 int main(void)
 {
@@ -28,17 +32,25 @@ int main(void)
 	double t;
 	
 	readInput();
+	t = GetTime();
 	A = (char **)malloc(sizeof(char *) * N);
+	Sort[0] = SelectionSort;
+	Sort[1] = InsertionSort;
+	Sort[2] = BubbleSort;
+	Sort[3] = ShakerSort;
 	for (i = 0; i < R; i++) {
 		copyArray(data, A);
-		// SelectionSort(A, N);
-		// InsertionSort(A, N);
-		// BubbleSort(A, N);
-		ShakerSort(A, N);
+		(*Sort[METHOD])(A, N);
 		if (i == 0) {
 			printArray(A);
 		}
 	}
+	t = (GetTime() - t) / R;
+	printf("%s Sort:\n", algNames[METHOD]);
+	printf("  N = %d\n", N);
+	printf("  CPU time = %.5e seconds\n", t);
+	freeMemory(data, N);
+	free(A);
 
 	return 0;
 }
@@ -73,6 +85,14 @@ void copyArray(char **data, char **A)
 	for (i = 0; i < N; i++) {
 		A[i] = data[i];
 	}
+}
+
+double GetTime(void)
+{
+	struct timeval tv;
+
+	gettimeofday(&tv, NULL);
+	return tv.tv_sec + 1e-6 * tv.tv_usec;
 }
 
 void SelectionSort(char **list, int n)
@@ -148,4 +168,14 @@ void swap(char **a, char **b)
 	tmp = *a;
 	*a = *b;
 	*b = tmp;
+}
+
+void freeMemory(char **list, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++) {
+		free(list[i]);
+	}
+	free(list);
 }
