42bf85bae4a9522053b6549e6e59af895868ddc4
Thomas <thomaschou861205@gmail.com>
2020-03-13 09:43:29 +0800

    - can read inputs

--- hw01/hw01.c
+++ hw01/hw01.c
@@ -1,33 +1,90 @@
-// EE3980 HW01 Quadratic Sorts 
-// 105061110 周柏宇
-// 2020/03/10
+// EE3980 HW01 Quadratic Sorts
+// 105061110, 周柏宇
+// 2020/03/12
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/time.h>
 
+int N;
+char **data;	// input data
+char **A;		//array to be sorted
+int R = 1;
+
+void readInput(void);
+void printArray(char **A);
+void copyArray(char **data, char **A);
+double GetTime(void);
 void SelectionSort(char **list, int n);
 void InsertionSort(char **list, int n);
 void BubbleSort(char **list, int n);
 void ShakerSort(char **list, int n);
-// double GetTime(void);
 
 int main(void)
 {
-    int i; // index
+	int i;
+	double t;
+	
+	readInput();
+	A = (char **)malloc(sizeof(char *) * N);
+	for (i = 0; i < R; i++) {
+		copyArray(data, A);
+		// SelectionSort(A, N);
+		if (i == 0) printArray(A);
+	}
 
-    // t = GetTime()
-    for (i = 0; i < 500; i++) {
-        ;
-    }
-    // printf("%f\n", t - GetTime());
-    return 0;
+	return 0;
 }
 
-// double GetTime(void)
-// {
-//     struct timeval tv;
+void readInput(void)
+{
+	int i;
+	char tmpWord[1000];
 
-//     gettimeofday(&tv, NULL);
-//     return tv.tv_sec + 1e-6 * tv.tv_usec;
-// }
+	scanf("%d", &N);
+	data = (char **)malloc(sizeof(char *) * N);
+	for (i = 0; i < N; i++) {
+		scanf("%s", tmpWord);
+		data[i] = (char *)malloc(sizeof(char) * (strlen(tmpWord) + 1));
+		strcpy(data[i], tmpWord);
+	}
+}
+
+void printArray(char **A)
+{
+	int i;
+
+	for (i = 0; i < N; i++) {
+		printf("%d %s\n", i + 1, A[i]);
+	}
+}
+
+void copyArray(char **data, char **A)
+{
+	int i;
+
+	for (i = 0; i < N; i++) {
+		A[i] = data[i];
+	}
+}
+
+void SelectionSort(char **list, int n)
+{
+	;
+}
+
+void InsertionSort(char **list, int n)
+{
+	;
+}
+
+void BubbleSort(char **list, int n)
+{
+	;
+}
+
+void ShakerSort(char **list, int n)
+{
+	;
+}
