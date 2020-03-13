9faa9883e73058bc1d947351fd322c3a92abba60
Thomas <thomaschou861205@gmail.com>
2020-03-13 13:38:40 +0800

    4 sorts algorithm done

--- hw01/hw01.c
+++ hw01/hw01.c
@@ -20,6 +20,7 @@ void SelectionSort(char **list, int n);
 void InsertionSort(char **list, int n);
 void BubbleSort(char **list, int n);
 void ShakerSort(char **list, int n);
+void swap(char **a, char **b);
 
 int main(void)
 {
@@ -31,7 +32,12 @@ int main(void)
 	for (i = 0; i < R; i++) {
 		copyArray(data, A);
 		// SelectionSort(A, N);
-		if (i == 0) printArray(A);
+		// InsertionSort(A, N);
+		// BubbleSort(A, N);
+		ShakerSort(A, N);
+		if (i == 0) {
+			printArray(A);
+		}
 	}
 
 	return 0;
@@ -71,20 +77,75 @@ void copyArray(char **data, char **A)
 
 void SelectionSort(char **list, int n)
 {
-	;
+	int i, j, k;
+
+	for (i = 0; i < n; i++) {
+		j = i;
+		for (k = i + 1; k < n; k++) {
+			if (strcmp(list[k], list[j]) < 0) {
+				j = k;
+			}
+		}
+		swap(&list[i], &list[j]);
+	}
 }
 
 void InsertionSort(char **list, int n)
 {
-	;
+	int j, i;
+	char *tmp;
+
+	for (j = 1; j < n; j++) {
+		tmp = list[j];
+		i = j - 1;
+		while ((i >= 0) && (strcmp(tmp, list[i]) < 0)) {
+			list[i + 1] = list[i];
+			i--;
+		}
+		list[i + 1] = tmp;
+	}
 }
 
 void BubbleSort(char **list, int n)
 {
-	;
+	int i, j;
+
+	for (i = 0; i < n - 1; i++) {
+		for (j = n - 1; j > i; j--) {
+			if (strcmp(list[j], list[j - 1]) < 0) {
+				swap(&list[j], &list[j - 1]);
+			}
+		}
+	}
 }
 
 void ShakerSort(char **list, int n)
 {
-	;
+	int j;
+	int l = 0;
+	int r = n - 1;
+
+	while (l <= r) {
+		for (j = r; j > l; j--) {
+			if (strcmp(list[j], list[j - 1]) < 0) {
+				swap(&list[j], &list[j - 1]);
+			}
+		}
+		l++;
+		for (j = l; j < r; j++) {
+			if (strcmp(list[j], list[j + 1]) > 0) {
+				swap(&list[j], &list[j + 1]);
+			}
+		}
+		r--;
+	}
+}
+
+void swap(char **a, char **b)
+{
+	char *tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
