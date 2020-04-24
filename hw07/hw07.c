dce46b44278e7a0157aba5e12515b57c566380d3
Thomas <thomaschou861205@gmail.com>
2020-04-24 13:20:46 +0800

    - hw07.c: first version done

--- hw07/hw07.c
+++ hw07/hw07.c
@@ -8,6 +8,10 @@
 // #include <sys/time.h>
 #include <time.h>
 
+// typedef struct sDFSInfo {
+// 	int p, d, f;
+// } DFSInfo;
+
 int n_names;
 int n_links;
 char **names;
@@ -22,8 +26,9 @@ int SCCs_ptr;
 void readData();
 int nameToIndex(char *name);
 void SCC();
-void DFS_Call();
-void DFS_d(int v);
+void DFS_Call(int **graph, int *order);
+void DFS_d(int **graph, int *order, int v);
+void InsertionSort(int *keys, int *values);
 
 int main(void)
 {
@@ -32,7 +37,7 @@ int main(void)
 
 	start = time(NULL);
 	readData();
-	DFS_Call();
+	SCC();
 	end = time(NULL);
 	// for (i = 0 ; i < n_names; i++) printf("%s\n", names[i]);
 	// printf("%d\n", nameToIndex(names[2]));
@@ -54,8 +59,8 @@ void readData()
 	names = (char **)malloc(sizeof(char *) * n_names);
 	for (i = 0; i < n_names; i++) {
 		scanf("%s", &tmp);
-		// names[i] = (char *)malloc(sizeof(char) * (3 * strlen(tmp) + 1));
-		names[i] = (char *)malloc(sizeof(char) * (strlen(tmp) + 1)); // test c0.dat
+		names[i] = (char *)malloc(sizeof(char) * (3 * strlen(tmp) + 1));
+		// names[i] = (char *)malloc(sizeof(char) * (strlen(tmp) + 1)); // test c0.dat
 		strcpy(names[i], tmp);
 	}
 
@@ -99,12 +104,36 @@ int nameToIndex(char *name)
 
 void SCC()
 {
-	;
+	int i;
+	int *keys;
+
+	keys = (int *)malloc(sizeof(int) * n_names);
+	for (i = 0; i < n_names; i++) {
+		keys[i] = i;
+		// printf("%s ", names[keys[i]]);
+	}
+	// printf("\n");
+
+	// printf("Before:\n");
+	// for (i = 0; i < n_names; i++) printf("f[%d] = %d ", i, f[i]);
+	// printf("\n");
+	DFS_Call(graph, keys);
+	InsertionSort(keys, f);
+	// printf("After:\n");
+	// for (i = 0; i < n_names; i++) {
+	// 	printf("f[%d] = %d ", keys[i], f[i]);
+	// }
+	// printf("\n");
+	// for (i = 0; i < n_names; i++) {
+	// 	printf("%s ", names[keys[i]]);
+	// }
+	// printf("\n");
+	DFS_Call(graphT, keys);
 }
 
-void DFS_Call()
+void DFS_Call(int **graph, int *order)
 {
-	int i;
+	int i, j;
 
 	visited = (int *)malloc(sizeof(int) * n_names);
 	p = (int *)malloc(sizeof(int) * n_names);
@@ -122,13 +151,15 @@ void DFS_Call()
 	time_DFS = 0;
 	SCCs_ptr = 0;
 	for (i = 0; i < n_names; i++) {
-		if (visited[i] == 0) {
+		j = order[i];
+		if (visited[j] == 0) {
 			SCCs[SCCs_ptr] = -1;
 			SCCs_ptr++;
-			DFS_d(i);
+			DFS_d(graph, order, j);
 		}
 	}
 	SCCs[SCCs_ptr] = -2;
+
 	for (i = 0; i < n_names * 2 && SCCs[i] != -2; i++) {
 		if (SCCs[i] != -1) printf("%s ", names[SCCs[i]]);
 		else printf("\n");
@@ -136,24 +167,46 @@ void DFS_Call()
 	printf("\n");
 }
 
-void DFS_d(int v)
+void DFS_d(int **graph, int *order, int v)
 {
-	int i;
+	int i, j;
 
 	visited[v] = 1;
-	time_DFS++;
+	time_DFS++; // time start at 1
 	d[v] = time_DFS;
 	SCCs[SCCs_ptr] = v;
 	SCCs_ptr++;
-	// printf("DFS_d %d d[%d] = %d\n", v + 1, v + 1, time_DFS);
+	// printf("DFS_d %s d[%d] = %d\n", names[v], v, time_DFS);
 	for (i = 0; i < n_names; i++) {
-		if ((graph[v][i] == 1) && (visited[i] == 0)) {
-			p[i] = v;
-			DFS_d(i);
+		j = order[i];
+		if ((graph[v][j] == 1) && (visited[j] == 0)) {
+			p[j] = v;
+			DFS_d(graph, order, j);
 		}
 	}
 	visited[v] = 2;
 	time_DFS = time_DFS + 1;
 	f[v] = time_DFS;
-	// printf("DFS_d %d f[%d] = %d\n", v + 1, v + 1, time_DFS);
+	// printf("DFS_d %s f[%d] = %d\n", names[v], v, time_DFS);
+}
+
+
+void InsertionSort(int *keys, int *values)
+{
+	int j, i;							// index
+	int tmp_key, tmp_value;				// temporary char pointer
+
+	for (j = 1; j < n_names; j++) {	// assume list[0 : j - 1] already sorted
+		tmp_key = keys[j];
+		tmp_value = values[j];				// copy the word at index j
+		i = j - 1;					// initialize i with j - 1
+		// repeat until list[i] is smaller 
+		while ((i >= 0) && (tmp_value > values[i])) {
+			keys[i + 1] = keys[i];
+			values[i + 1] = values[i];	// fill the previous word with current word
+			i--;					// move on to the next word
+		}
+		keys[i + 1] = tmp_key;			// fill the word list[j] at index i + 1
+		values[i + 1] = tmp_value;
+	}
 }
