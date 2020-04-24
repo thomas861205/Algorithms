43d92ea828162bc5d04a559043fc6bbb2a100432
Thomas <thomaschou861205@gmail.com>
2020-04-25 02:16:22 +0800

    - hw07.c: Insertion sort -> Merge Sort

--- hw07/hw07.c
+++ hw07/hw07.c
@@ -2,6 +2,15 @@
 // 105061110, 周柏宇
 // 2020/04/24
 
+// potential improvement
+// space:
+// adjacent matrix -> link list
+// graphT
+// SCCs
+// 
+// time: other sorting algorithm
+// 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,13 +21,19 @@
 // 	int p, d, f;
 // } DFSInfo;
 
+typedef struct node {
+	int data;
+	struct node *next;
+} Node;
+
 int n_names;
 int n_links;
+int n_SCCs;
 char **names;
 int **graph;
 int **graphT; // transpose graph
 int *visited;
-int *p, *d, *f;
+int *f;
 int time_DFS;
 int *SCCs;
 int SCCs_ptr;
@@ -28,30 +43,43 @@ int nameToIndex(char *name);
 void SCC();
 void DFS_Call(int **graph, int *order);
 void DFS_d(int **graph, int *order, int v);
-void InsertionSort(int *keys, int *values);
+void InsertionSort(int *keys, int *values, int low, int high);
+void MergeSort(int *keys, int *values, int low, int high);
+void Merge(int *keys, int *values, int low, int mid, int high);
 
 int main(void)
 {
 	int i;
-	time_t start, end;
+	int subgroup_idx = 0;
+	clock_t start, end;
+	double time_elapsed;
 
-	start = time(NULL);
 	readData();
+	start = clock();
 	SCC();
-	end = time(NULL);
-	// for (i = 0 ; i < n_names; i++) printf("%s\n", names[i]);
-	// printf("%d\n", nameToIndex(names[2]));
-	// for (i = 0; i < n_names; i++) printf("%d ", d[i]);
-	// printf("\n");
-	// for (i = 0; i < n_names; i++) printf("%d ", f[i]);
-	// printf("\n");
-	printf("%lf s\n", difftime(end, start));
+	end = clock();
+	time_elapsed = ((double) (end - start)) / CLOCKS_PER_SEC;
+	printf("N = %d M = %d CPU time = %.5e\n", n_names, n_links, time_elapsed);
+	printf("Number of subgroups: %d\n", n_SCCs);
+	for (i = 0; i < n_names * 2 && SCCs[i] != -2; i++) {
+		if (i == 0) {
+			printf("  Subgroup %d:", ++subgroup_idx);
+		}
+		else if (SCCs[i] == -1) {
+			printf("\n  Subgroup %d:", ++subgroup_idx);
+		}
+		else {
+			printf(" %s", names[SCCs[i]]);
+		}
+	}
+	printf("\n");
+
 	return 0;
 }
 
 void readData()
 {
-	int i, j;
+	int i, j, k;
 	char tmp[20], tmp2[20], tmp3[20];
 
 	scanf("%d", &n_names);
@@ -74,11 +102,14 @@ void readData()
 			graphT[i][j] = 0;
 		}
 	}
+
 	for (i = 0; i < n_links; i++) {
 		scanf("%s %s %s", &tmp, &tmp2, &tmp3);
 		// printf("%d -> %d\n", nameToIndex(tmp), nameToIndex(tmp3));
-		graph[nameToIndex(tmp)][nameToIndex(tmp3)] = 1;
-		graphT[nameToIndex(tmp3)][nameToIndex(tmp)] = 1;
+		j = nameToIndex(tmp);
+		k = nameToIndex(tmp3);
+		graph[j][k] = 1;
+		graphT[k][j] = 1;
 	}
 
 	// for (i = 0; i < n_names; i++) {
@@ -114,20 +145,15 @@ void SCC()
 	}
 	// printf("\n");
 
-	// printf("Before:\n");
-	// for (i = 0; i < n_names; i++) printf("f[%d] = %d ", i, f[i]);
-	// printf("\n");
 	DFS_Call(graph, keys);
-	InsertionSort(keys, f);
-	// printf("After:\n");
-	// for (i = 0; i < n_names; i++) {
-	// 	printf("f[%d] = %d ", keys[i], f[i]);
-	// }
-	// printf("\n");
+	// InsertionSort(keys, f);
+	MergeSort(keys, f, 0, n_names - 1);
+
 	// for (i = 0; i < n_names; i++) {
 	// 	printf("%s ", names[keys[i]]);
 	// }
 	// printf("\n");
+
 	DFS_Call(graphT, keys);
 }
 
@@ -136,35 +162,27 @@ void DFS_Call(int **graph, int *order)
 	int i, j;
 
 	visited = (int *)malloc(sizeof(int) * n_names);
-	p = (int *)malloc(sizeof(int) * n_names);
-	d = (int *)malloc(sizeof(int) * n_names);
 	f = (int *)malloc(sizeof(int) * n_names);
 	SCCs = (int *)malloc(sizeof(int) * n_names * 2);
 	for (i = 0; i < n_names; i++) {
 		visited[i] = 0;
-		p[i] = 0;
-		d[i] = 0;
 		f[i] = 0;
 		SCCs[2 * i] = 0;
 		SCCs[2 * i + 1] = 0;
 	}
 	time_DFS = 0;
 	SCCs_ptr = 0;
+	n_SCCs = 0;
 	for (i = 0; i < n_names; i++) {
 		j = order[i];
 		if (visited[j] == 0) {
 			SCCs[SCCs_ptr] = -1;
 			SCCs_ptr++;
+			n_SCCs++;
 			DFS_d(graph, order, j);
 		}
 	}
 	SCCs[SCCs_ptr] = -2;
-
-	for (i = 0; i < n_names * 2 && SCCs[i] != -2; i++) {
-		if (SCCs[i] != -1) printf("%s ", names[SCCs[i]]);
-		else printf("\n");
-	}
-	printf("\n");
 }
 
 void DFS_d(int **graph, int *order, int v)
@@ -173,40 +191,83 @@ void DFS_d(int **graph, int *order, int v)
 
 	visited[v] = 1;
 	time_DFS++; // time start at 1
-	d[v] = time_DFS;
 	SCCs[SCCs_ptr] = v;
 	SCCs_ptr++;
 	// printf("DFS_d %s d[%d] = %d\n", names[v], v, time_DFS);
 	for (i = 0; i < n_names; i++) {
 		j = order[i];
 		if ((graph[v][j] == 1) && (visited[j] == 0)) {
-			p[j] = v;
 			DFS_d(graph, order, j);
 		}
 	}
 	visited[v] = 2;
-	time_DFS = time_DFS + 1;
+	time_DFS++;
 	f[v] = time_DFS;
 	// printf("DFS_d %s f[%d] = %d\n", names[v], v, time_DFS);
 }
 
 
-void InsertionSort(int *keys, int *values)
+void InsertionSort(int *keys, int *values, int low, int high)
 {
 	int j, i;							// index
 	int tmp_key, tmp_value;				// temporary char pointer
 
-	for (j = 1; j < n_names; j++) {	// assume list[0 : j - 1] already sorted
+	for (j = low + 1; j <= high; j++) {	// assume list[0 : j - 1] already sorted
 		tmp_key = keys[j];
 		tmp_value = values[j];				// copy the word at index j
 		i = j - 1;					// initialize i with j - 1
 		// repeat until list[i] is smaller 
-		while ((i >= 0) && (tmp_value > values[i])) {
+		while ((i >= low) && (tmp_value > values[i])) {
 			keys[i + 1] = keys[i];
 			values[i + 1] = values[i];	// fill the previous word with current word
 			i--;					// move on to the next word
 		}
 		keys[i + 1] = tmp_key;			// fill the word list[j] at index i + 1
 		values[i + 1] = tmp_value;
+	}
+}
+
+void MergeSort(int *keys, int *values, int low, int high)
+{
+	int mid;
+
+	if (high - low > 0) InsertionSort(keys, values, low, high);
+	else {
+		mid = (low + high) / 2;
+		MergeSort(keys, values, low, mid);
+		MergeSort(keys, values, mid + 1, high);
+		Merge(keys, values, low, mid, high);
+	}
+}
+
+void Merge(int *keys, int *values, int low, int mid, int high)
+{
+	int k;
+	int h = low; // index for lower half
+	int i = low; // index for B
+	int j = mid + 1; // index for upper half
+
+	while ((h <= mid) && (j <= high)) {
+		if (values[h] >= values[j]) {
+			keys[i] = h;
+			h++;
+		}
+		else {
+			keys[i] = j;
+			j++;
+		}
+		i++;
+	}
+	if (h > mid) {
+		for (k = j; k <= high; k++) {
+			keys[i] = k;
+			i++;
+		}
+	}
+	else {
+		for (k = h; k <= mid; k++) {
+			keys[i] = k;
+			i++;
+		}
 	}
 }
