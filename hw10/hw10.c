26fc10e459b1d4732bdfde5062aa9897a456dc41
Thomas <thomaschou861205@gmail.com>
2020-05-21 18:14:39 +0800

    pee break

--- hw10/hw10.c
+++ hw10/hw10.c
@@ -4,50 +4,54 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
+#include <time.h>
+// #include <sys/time.h>
+#define MAX 99
 
 int g_C[] = {1, 5, 10, 50};
-int x[4][100];
-int g[4][100];
+int x[4][MAX + 1];
+int g[4][MAX + 1];
 
 void P1(void);
 void P2(void);
 void P3(void);
 void P4(void);
 int g_TD(int n, int D);
-int g_BU(int n, int D);
+int g_BU(int n, int D, int skip);
 int g_sol(int n, int D, int C[]);
 void verify(int C[]);
+double GetTime(void); // get local time in seconds
 
 int main(void)
 {
 	int i, j, k;
+	clock_t start, end;
 
 	for (j = 0; j < 4; j++) { // # choice of coins
 		g[j][0] = 0; // value = 0
 		x[j][0] = 0;
 		if (j == 0) { // $1
-			for (k = 1; k <= 99; k++) {
+			for (k = 1; k <= MAX; k++) {
 				g[j][k] = k;
 				x[j][k] = k;
 			}
 		}
 		else {
-			for (k = 1; k <= 99; k++) {
+			for (k = 1; k <= MAX; k++) {
 				g[j][k] = -1;
 				x[j][k] = -1;
 			}
 		}
 	}
-
-	// verify(g_C);
-	// k = 99;
-	// printf("%d\n", g_TD(4, k));
-	// printf("%d\n", g_BU(4, k));
+	start = clock();
 	P1();
 	P2();
 	P3();
 	P4();
-	for (j = 0; j < 4; j++) free(g[j]);
+	end = clock();
+	verify(g_C);
+	printf("%.5fs\n", ((double) (end - start)) / CLOCKS_PER_SEC);
 	return 0;
 }
 
@@ -56,56 +60,56 @@ void P1(void)
 	int i;
 	int sum = 0;
 
-	for (i = 1; i <= 99; i++) {
-		// sum += g_TD(4, i);
-		sum += g_BU(4, i);
+	for (i = 1; i <= MAX; i++) {
+		sum += g_TD(4, i);
+		// sum += g_BU(4, i, 1);
 	}
 	printf("For coin set {1, 5, 10, 50} the average is %.5f\n",
-		(float)sum / 99);
+		(float)sum / MAX);
 }
 
 void P2(void)
 {
 	int i, j;
 	int sum;
-	int min = 100000000;
+	int min = INT_MAX;
 	int minCoin = -1;
 
-	for (i = 51; i <= 99; i++) {
+	for (i = 51; i <= MAX; i++) {
 		g_C[3] = i;
 		sum = 0;
-		for (j = 1; j <= 99; j++) g[3][j] = -1;
-		for (j = 1; j <= 99; j++) {
-			// sum += g_TD(4, j);
-			sum += g_BU(4, j);
+		for (j = 1; j <= MAX; j++) {
+			g[3][j] = -1; // init
+			sum += g_TD(4, j);
+			// sum += g_BU(4, j, 3);
 		}
 		if (sum < min) {
-			min = sum;
-			minCoin = i;
+			min = sum; // record min # coins
+			minCoin = i; // $i
 		}
 	}
-	g_C[3] = 50;
+	g_C[3] = 50; // restore coin set
 	printf("Coin set {1, 5, 10, %d} has the minimum average of %.5f\n", 
-		minCoin, (float)min / 99);
+		minCoin, (float)min / MAX);
 }
 
 void P3(void)
 {
 	int i, j;
 	int sum;
-	int min = 100000000;
+	int min = INT_MAX;
 	int minCoin = -1;
 
 	for (i = 6; i <= 49; i++) {
 		g_C[2] = i;
 		sum = 0;
-		for (j = 1; j <= 99; j++) {
+		for (j = 1; j <= MAX; j++) {
 			g[2][j] = -1;
 			g[3][j] = -1;
 		}
-		for (j = 1; j <= 99; j++) {
-			// sum += g_TD(4, j);
-			sum += g_BU(4, j);
+		for (j = 1; j <= MAX; j++) {
+			sum += g_TD(4, j);
+			// sum += g_BU(4, j, 2);
 		}
 		if (sum < min) {
 			min = sum;
@@ -113,7 +117,7 @@ void P3(void)
 		}
 	}
 	printf("Coin set {1, 5, %d, 50} has the minimum average of %.5f\n",
-		minCoin, (float)min / 99);
+		minCoin, (float)min / MAX);
 	g_C[2] = 10;
 }
 
@@ -121,22 +125,22 @@ void P4(void)
 {
 	int i, j, k, l;
 	int sum;
-	int min = 100000000;
+	int min = INT_MAX;
 	int minCoinA = -1;
 	int minCoinB = -1;
 
 	for (i = 6; i <= 98; i++) {
-		for (j = i + 1; j <= 99; j++) {
+		for (j = i + 1; j <= MAX; j++) {
 			g_C[2] = i;
 			g_C[3] = j;
 			sum = 0;
-			for (k = 1; k <= 99; k++) {
+			for (k = 1; k <= MAX; k++) {
 				g[2][k] = -1;
 				g[3][k] = -1;
 			}
-			for (k = 1; k <= 99; k++) {
+			for (k = 1; k <= MAX; k++) {
 				sum += g_TD(4, k);
-				// sum += g_BU(4, k);
+				// sum += g_BU(4, k, 2);
 			}
 			if (sum < min) {
 				min = sum;
@@ -146,7 +150,7 @@ void P4(void)
 		}
 	}
 	printf("Coin set {1, 5, %d, %d} has the minimum average of %.5f\n", 
-		minCoinA, minCoinB, (float)min / 99);
+		minCoinA, minCoinB, (float)min / MAX);
 	g_C[2] = 10;
 	g_C[3] = 50;
 }
@@ -157,26 +161,25 @@ int g_TD(int n, int D)
 	int tmp, min;
 
 	if (g[n - 1][D] >= 0) return g[n - 1][D];
-	else {
-		min = g_TD(n - 1, D);
-		for (i = 1; i <= (D / g_C[n - 1]); i++) {
-			tmp = i + g_TD(n - 1, D - i * g_C[n - 1]);
-			if (tmp < min) min = tmp;
-		}
+	min = g_TD(n - 1, D);
+	for (i = 1; i <= (D / g_C[n - 1]); i++) {
+		tmp = i + g_TD(n - 1, D - i * g_C[n - 1]);
+		if (tmp < min) min = tmp;
 	}
 	g[n - 1][D] = min;
+
 	return min;
 }
 
-int g_BU(int n, int D)
+int g_BU(int n, int D, int skip)
 {
 	int i, j, k;
 	int tmp, min;
 
-	for (i = 1; i < n; i++) { // # choice of coins
+	for (i = skip; i < n; i++) { // # choice of coins
 		for (j = 1; j <= D; j++) { // total value
 			min = g[i - 1][j];
-			for (k = 0; k <= (j / g_C[i]); k++) {
+			for (k = 1; k <= (j / g_C[i]); k++) {
 				tmp = k + g[i - 1][j - k * g_C[i]];
 				if (tmp < min) min = tmp;
 			}
@@ -225,7 +228,16 @@ void verify(int C[])
 	int ans;
 
 	printf(" D: %2d %2d %2d %2d ans\n", C[0], C[1], C[2], C[3]);
-	for (i = 1; i <= 99; i++) {
+	for (i = 1; i <= MAX; i++) {
 		ans = g_sol(4, i, C);
 	}
 }
+
+// double GetTime(void)						// get local time in seconds
+// {
+// 	struct timeval tv;						// variable to store time
+
+// 	gettimeofday(&tv, NULL);				// get local time
+
+// 	return tv.tv_sec + 1e-6 * tv.tv_usec;	// return local time in seconds
+// }
