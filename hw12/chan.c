20473010eaa59b8adc002ca46257504445491539
Thomas <thomaschou861205@gmail.com>
2020-06-06 17:47:13 +0800

    - chan.c: partially commented

--- hw12/chan.c
+++ hw12/chan.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <sys/time.h>
 #define INF 1000000
 
 typedef struct node {
@@ -9,15 +10,16 @@ typedef struct node {
 	int R;
 } NODE;
 
-int N;
-char **city;
-int **cost;
-int *visited;
-int LB = INF;
+int N; // number of cities
+char **city; // name of the cities
+int **cost; // cost table
+int *visited; // record if the city is visited
+int LB = INF; // lower bound of the cost
 int n_step = 0;
 int *row_avl;
 int *col_avl;
 int *ans;
+int *ans_tmp;
 
 void ReadandInit(void);
 void printCost(int **cost);
@@ -27,15 +29,12 @@ void DFS_Call(void);
 void DFS_d(int nth, int u, int R, int **cost);
 int RowR(int **cost, int doit);
 int ColR(int **cost, int doit);
-int RowCf(int **cost, int *min);
-int ColCf(int **cost, int *min);
-void array2heap(NODE *A, int n);
-void heapify(NODE *A, int i, int n);
-int nextH(NODE *A, int n, NODE *ret);
 int nextA(NODE *A, int n, NODE *ret);
+void freeAll(void);
 
 int main(void)
 {
+	int i;
 	clock_t start, end;
 
 	ReadandInit();
@@ -43,8 +42,12 @@ int main(void)
 	DFS_Call();
 	end = clock();
 	printf("%d steps\n", n_step);
-	printf("total cost is %d.\n", LB);
 	printf("%.5lfs\n", (double)(end - start) / CLOCKS_PER_SEC);
+	printf("Minimum distance route:\n");
+	for (i = 0; i < N; i++) {
+		printf(" %s -> %s\n", city[ans[i]], city[ans[(i + 1) % N]]);
+	}
+	printf("Total travelling distance: %d\n", LB);
 	return 0;
 }
 
@@ -64,7 +67,6 @@ void ReadandInit(void)
 		tmp[j] = '\0';
 		city[i] = (char *)malloc(sizeof(char) * (strlen(tmp) + 1));
 		strcpy(city[i], tmp);
-		// printf("%s\n", city[i]);
 	}
 	cost = (int **)malloc(sizeof(int *) * N);
 	for (i = 0; i < N; i++) {
@@ -77,6 +79,8 @@ void ReadandInit(void)
 	visited = (int *)malloc(sizeof(int) * N);
 	row_avl = (int *)malloc(sizeof(int) * N);
 	col_avl = (int *)malloc(sizeof(int) * N);
+	ans = (int *)malloc(sizeof(int) * N);
+	ans_tmp = (int *)malloc(sizeof(int) * N);
 	for (i = 0; i < N; i++) {
 		visited[i] = 0;
 		row_avl[i] = 1;
@@ -90,7 +94,6 @@ void printCost(int** cost)
 
 	for (i = 0; i < N; i++) {
 		for (j = 0; j < N; j++) {
-			// if (cost[i][j] == INF) printf("  x ");
 			if (!row_avl[i] || !col_avl[j] || cost[i][j] == INF)
 				printf("  x ");
 			else printf("%3d ", cost[i][j]);
@@ -118,12 +121,12 @@ void DFS_Call(void)
 	int R;
 	int **cost_cpy;
 
-	visited[0] = 1; // node 0 has been visited
-	cost_cpy = copy(cost);
-	R = RowR(cost_cpy, 1) + ColR(cost_cpy, 1);
-	n_step++;
+	visited[0] = 1; // city 0 has been visited
+	cost_cpy = copy(cost); // a duplicate of cost table
+	R = RowR(cost_cpy, 1) + ColR(cost_cpy, 1); // row and column reduce
+	n_step++; // number of steps (calculation of lower bound) increase by 1
 	DFS_d(1, 0, R, cost_cpy);
-	freeCopy(cost_cpy);
+	freeCopy(cost_cpy); // free allocated memory
 }
 
 void DFS_d(int nth, int u, int R, int **cost)
@@ -139,6 +142,7 @@ void DFS_d(int nth, int u, int R, int **cost)
 	NODE *order = (NODE *)malloc(sizeof(NODE) * (N - nth));
 
 	row_avl[u] = 0; // set row u to INF
+	ans_tmp[nth - 1] = u;
 	for (i = 0; i < N; i++) {
 		if (!visited[i]) { // try all available cities
 			col_avl[i] = 0; // set column i to INF
@@ -146,8 +150,10 @@ void DFS_d(int nth, int u, int R, int **cost)
 			cost[i][u] = INF; // set cost[i][u] to INF
 
 			order[i_order].idx = i;
+			// calculate the lower bound
 			order[i_order].R = R + cost[u][i] + RowR(cost, 0) + ColR(cost, 0);
-			n_step++; // number of steps (calculation of lower bound) increase by 1
+			// number of steps (calculation of lower bound) increase by 1
+			n_step++;
 			// printf("%d. %d %d\n", n_step, i + 1, order[i_order].R);
 			i_order++;
 
@@ -156,17 +162,17 @@ void DFS_d(int nth, int u, int R, int **cost)
 		}
 	}
 	if (nth == N - 1) { // lowest non-leaf node
+		ans_tmp[nth] = order[0].idx;
 		row_avl[u] = 1; // recover row u
 		tmp = order[0].R;
-		// printf("done %d %d\n", LB, tmp);
-		LB = tmp < LB ? tmp : LB; // update lower bound
+		if (tmp < LB) {
+			LB = tmp; // update lower bound
+			for (i = 0; i < N; i++) ans[i] = ans_tmp[i];
+		}
 		free(order);
 		return;
 	}
-
-	// array2heap(order, i_order); // make the array 'order' a min heap
-
-	// while (((status = nextH(order, i_order--, &min) != -1)) && (min.R <= LB)) {
+	// try next city according to least-cost, stop when dominated
 	while (((status = nextA(order, i_order--, &min) != -1)) && (min.R <= LB)) {
 		v = min.idx;
 		visited[v] = 1; // set city v as visited
@@ -183,50 +189,6 @@ void DFS_d(int nth, int u, int R, int **cost)
 	}
 	row_avl[u] = 1; // recover row u
 	free(order);
-}
-
-void array2heap(NODE *A, int n)
-{
-	int i;
-
-	for (i = n / 2 - 1; i >= 0; i--) {
-		heapify(A, i, n);
-	}
-}
-
-void heapify(NODE *A, int i, int n)
-{
-	int j, done;
-	NODE tmp;
-
-	j = 2 * (i + 1) - 1;
-	tmp = A[i];
-	done = 0;
-	while ((j <= n - 1) && (!done)) {
-		if ((j < n - 1) && (A[j].R > A[j + 1].R)) j++;
-		if (tmp.R < A[j].R) done = 1;
-		else {
-			A[(j + 1) / 2 - 1] = A[j];
-			j = 2 * (j + 1) - 1;
-		}
-	}
-	A[(j + 1) / 2 - 1] = tmp;
-}
-
-int nextH(NODE *A, int n, NODE *ret) // return next minimum in heap
-{
-	if (n == 1) {
-		// only one element in heap, no need to swap and heapify
-		*ret = A[0];
-		return 0;
-	}
-	else if (n > 1) {
-		// more than one element, need to swap
-		*ret = A[0]; A[0] = A[n - 1]; A[n - 1] = *ret;
-		heapify(A, 0, n - 1);
-		return 0;
-	}
-	else return -1;
 }
 
 int nextA(NODE *A, int n, NODE *ret){
@@ -251,25 +213,27 @@ int nextA(NODE *A, int n, NODE *ret){
 
 int RowR(int **cost, int doit)
 {
-	int i, j;
-	int min;
-	int s_min = 0;
+	int i, j; // loop indices
+	int min; // minimum of the row
+	int s_min = 0; // cumulated minimum
 
 	for (i = 0; i < N; i++) {
 		if (row_avl[i]) {
 			min = INF;
 			for (j = 0; j < N; j++) {
-				if (col_avl[j] && (cost[i][j] < min))
-					min = cost[i][j];
+				if (col_avl[j] && (cost[i][j] < min)) {
+					min = cost[i][j]; // update the minimum of the row
+				}
 			}
 			if (min != INF) {
-				if (doit) {
+				if (doit) { // update the table of cost
 					for (j = 0; j < N; j++) {
-						if (col_avl[j] && cost[i][j] != INF)
+						if (col_avl[j] && cost[i][j] != INF) {
 							cost[i][j] -= min;
+						}
 					}
 				}
-				s_min += min;
+				s_min += min; // accumulate the minimum
 			}
 		}
 	}
@@ -278,35 +242,34 @@ int RowR(int **cost, int doit)
 
 int ColR(int **cost, int doit)
 {
-	int i, j;
-	int min;
-	int s_min = 0;
+	int i, j; // loop indices
+	int min; // minimum of the column
+	int s_min = 0; // cumulated minimum
 
 	for (i = 0; i < N; i++) {
 		if (col_avl[i]) {
 			min = INF;
 			for (j = 0; j < N; j++) {
-				if (row_avl[j] && (cost[j][i] < min))
-					min = cost[j][i];
+				if (row_avl[j] && (cost[j][i] < min)) {
+					min = cost[j][i]; // update the minimum of the column
+				}
 			}
 			if (min != INF) {
-				if (doit) {
+				if (doit) { // update the table of cost
 					for (j = 0; j < N; j++) {
-						if (row_avl[j] && cost[j][i] != INF)
+						if (row_avl[j] && cost[j][i] != INF) {
 							cost[j][i] -= min;
+						}
 					}
 				}
-				s_min += min;
+				s_min += min; // accumulate the minimum
 			}
 		}
 	}
 	return s_min;
 }
 
-// int RowCf(int **cost, int *min);
-// int ColCf(int **cost, int *min);
-
-void freeCopy(int **cost)
+void freeCopy(int **cost) // free duplicate of cost table
 {
 	int i;
 
